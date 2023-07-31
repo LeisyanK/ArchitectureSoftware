48e63f20a6f927ad940f3eca6f0381e67801928c
Kashapova Leisyan <LeisyanRA@mail.ru>
2023-07-31 20:16:51 +0500

    HW seminar1

--- Editor3D/src/InMemoryModel/ModelStore.java
+++ Editor3D/src/InMemoryModel/ModelStore.java
@@ -2,10 +2,26 @@ package InMemoryModel;
 
 import java.util.List;
 
-public class ModelStore {
-    public List<PoligonModel> models;
+import ModelElements.Camera;
+import ModelElements.Flash;
+import ModelElements.Poligon;
+import ModelElements.Scene;
+
+public class ModelStore implements iModelChanger{
+    public List<Poligon> models;
     public List<Scene> scenes;
     public List<Flash> flashes;
     public List<Camera> cameras;
     private List<iModelChangeObserver> changeObservers;
+
+    public Scene getScena(int var1){
+        return new Scene();
+    }
+
+    @Override
+    public void notifyChange(iModelChanger sender) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notifyChange'");
+    }
+
 }
---
+++ Editor3D/src/ModelElements/Scene.java
@@ -0,0 +1,11 @@
+package ModelElements;
+
+import java.util.List;
+
+public class Scene {
+    public int id;
+    public List<PoligonalModel> models;
+    public List<Flash> flashes;
+
+    
+}
